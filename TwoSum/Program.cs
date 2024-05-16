110fd8a9860ed3aeff43f374454a210d579232ef
Sumia Akter <sumia.017@gmail.com>
2024-05-16 19:46:02 +0100

    - restructured the project - added twosum solution

---
+++ TwoSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+var result = new Solution().TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+Console.WriteLine($"[{string.Join(", ", result)}]");
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new();
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (dict.TryGetValue(diff, out int index))
+            {
+                return new[] { index, diff };
+            }
+
+            if (!dict.ContainsKey(num))
+            {
+                dict.Add(num, i);
+            }
+        }
+
+        return new int[2];
+    }
+}
